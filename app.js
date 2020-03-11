3283d5d5142857061092aa5f68298ff28f022593
monty <montytan2000@gmail.com>
2020-03-11 09:46:43 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 		= require("./routes/index");
 
+console.log(process.env.DATABASEURL);
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-mongoose.connect("mongodb+srv://monty:tymontytan1@cluster0-qzbky.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
-	console.log("Connected to DB!");
-}).catch(err => {
-	console.log("ERROR:", err.message);
-});
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+// mongoose.connect("mongodb+srv://monty:tymontytan1@cluster0-qzbky.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
+// 	console.log("Connected to DB!");
+// }).catch(err => {
+// 	console.log("ERROR:", err.message);
+// });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
